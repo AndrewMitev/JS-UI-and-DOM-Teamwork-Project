9db0c0967b68339bf3efeb332c29316c8f8ae2c0
shunobaka <vesselin_minev@abv.bg>
2015-07-22 20:16:20 +0300

    Added constants variable and player module
    Added constants variable for storing constant values and player module
    used to create a player instance. Contains functions for updating the
    player movement angle, modifiers and position.

---
+++ script.js
@@ -0,0 +1,82 @@
+var gamefield = document.getElementById('game-field'),
+	gamefieldCtx = gamefield.getContext('2d');
+
+var CONSTANTS = {
+    PLAYER_RADIUS: 5
+};
+
+var player = (function() {
+    function changeMovementAngle(player) {
+        if (player.isRightPressed) {
+            player.movementAngle += 4 ;
+        } else if (player.isLeftPressed) {
+            player.movementAngle -= 4;
+        }
+    }
+
+    function changeModifiers(player) {
+        if (0 <= player.movementAngle && player.movementAngle <= 90) {
+            player.yModifier = player.movementAngle / 90;
+            player.xModifier = (90 - player.movementAngle) / 90;
+        } else if (90 < player.movementAngle && player.movementAngle <= 180) {
+            player.yModifier = (180 - player.movementAngle) / 90;
+            player.xModifier = (90 - player.movementAngle) / 90;
+        } else if (180 < player.movementAngle && player.movementAngle <= 270) {
+            player.yModifier = (180 - player.movementAngle) / 90;
+            player.xModifier = (player.movementAngle - 270) / 90;
+        } else {
+            player.yModifier = (player.movementAngle - 360) / 90;
+            player.xModifier = (player.movementAngle - 270) / 90;
+        }
+    }
+
+    function changePosition(player) {
+        player.x += 2 * player.xModifier;
+        player.y += 2 * player.yModifier;
+    }
+
+    var currentId = 0,
+        fieldWidth = +gamefield.getAttribute('width');
+    	fieldHeight = +gamefield.getAttribute('height');
+    	player = Object.create({});
+
+    Object.defineProperty(player, 'init', {
+        value: function(name) {
+            this.x = parseInt(Math.random() * fieldWidth);
+            this.y = parseInt(Math.random() * fieldHeight);
+            this.xModifier = 1;
+            this.yModifier = 0;
+            this.id = ++currentId;
+            this.name = name;
+            this.movementAngle = 0;
+            this.isLeftPressed = false;
+            this.isRightPressed = false;
+            return this;
+        }
+    });
+
+    Object.defineProperty(player, 'movementAngle', {
+        get: function() {
+            return this._movementAngle;
+        },
+        set: function(value) {
+            if (value < 0) {
+                this._movementAngle = 360 - 4;
+            } else if (value > 360) {
+                this._movementAngle = 0 + 4;
+            } else {
+                this._movementAngle = value;
+            }
+        }
+    });
+
+    Object.defineProperty(player, 'update', {
+        value: function() {
+            changeMovementAngle(this);
+            changeModifiers(this);
+            changePosition(this);
+        }
+    });
+
+    return player;
+}());
