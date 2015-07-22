b0e324681e45d36010004df3a8372aca48ef9f8e
shunobaka <vesselin_minev@abv.bg>
2015-07-22 20:17:45 +0300

    Created two test players and added event listeners

--- script.js
+++ script.js
@@ -80,3 +80,38 @@ var player = (function() {
 
     return player;
 }());
+
+var player1 = Object.create(Player).init('PlayerOne');
+var player2 = Object.create(Player).init('PlayerTwo');
+
+document.addEventListener('keydown', function(ev) {
+    if (ev.keyCode == 68) {
+        player1.isRightPressed = true;
+    } else if (ev.keyCode == 65) {
+        player1.isLeftPressed = true;
+    }
+}, false);
+
+document.addEventListener('keyup', function(ev) {
+    if (ev.keyCode == 68) {
+        player1.isRightPressed = false;
+    } else if (ev.keyCode == 65) {
+        player1.isLeftPressed = false;
+    }
+}, false);
+
+document.addEventListener('keydown', function(ev) {
+    if (ev.keyCode == 39) {
+        player2.isRightPressed = true;
+    } else if (ev.keyCode == 37) {
+        player2.isLeftPressed = true;
+    }
+}, false);
+
+document.addEventListener('keyup', function(ev) {
+    if (ev.keyCode == 39) {
+        player2.isRightPressed = false;
+    } else if (ev.keyCode == 37) {
+        player2.isLeftPressed = false;
+    }
+}, false);
